6d5d209218bc0b32f9fd8a25dc8aabc1efa9bd4c
Tiffany Zhu <tiffanyzhu29@gmail.com>
2018-10-23 22:59:36 -0400

    new commits

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+function changeItem(selectId, imageId) {
+     var dropdown = document.getElementById(selectId);
+     var image = document.getElementById(imageId);
+     image.src = dropdown.options[dropdown.selectedIndex].value;
+}
 
-function changeItem() {
-  var dropdown = document.getElementById("color-picker");
-  var image = document.getElementById("product-img");
-  image.src = dropdown.options[dropdown.selectedIndex].value
+/*Pseudo-code
+On event "click add to cart," retrieve the selected
+element by ID and the imageID and price and qty and
+and store as an array in localstorage
+
+Then on the cart page, retrieve all items in
+localstorage and display all items
+
+Then sum all the prices, multiplied by quantity
+for cart total */
+
+function addItem() {
+    var cartList = [];
+    cartList.push(options[dropdown.selectedIndex].text);
 }
