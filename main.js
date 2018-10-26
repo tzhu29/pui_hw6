354e06a64e60f8809bc5e1d70b521e40ca22b20f
Tiffany Zhu <tiffanyzhu29@gmail.com>
2018-10-25 22:21:19 -0400

    commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//localStorage.clear();
 
 function changeItem(selectId, imageId) {
      var dropdown = document.getElementById(selectId);
@@ -6,30 +5,7 @@ function changeItem(selectId, imageId) {
      image.src = dropdown.options[dropdown.selectedIndex].value;
 }
 
-/*Pseudo-code
-On event "click add to cart," retrieve the selected
-element by ID and the imageID and price and qty and
-and store as an array in localstorage
-
-Then on the cart page, retrieve all items in
-localstorage and display all items
-
-Then sum all the prices, multiplied by quantity
-for cart total
-
 var cartList = [];
-function addItem(selectId) {
-    var dropdown = document.getElementById(selectId);
-    var cartItem = {
-    "name": "dfg",
-    "qty": 3,
-    "fill": dropdown.options[dropdown.selectedIndex].text
-};
-
-    */
-
-var cartList = [];
-
 function addItem(colorSelectId, fillSelectId, qtySelectId) {
     var dropdownColor = document.getElementById(colorSelectId);
     var dropdownFill = document.getElementById(fillSelectId);
@@ -46,20 +22,14 @@ function addItem(colorSelectId, fillSelectId, qtySelectId) {
 };
 
 
-/*function pushToCart() {
-    localStorage.setItem('items', JSON.stringify(cartList));
-    var checkoutCart = JSON.parse(localStorage.getItem('items'));
-}*/
-var currentCart;
-// doc.ready
 function openCart() {
     var cartStorage = localStorage.getItem('items');
-    console.log(cartStorage);
+    console.log("cart storage [unparsed]:", cartStorage);
     if (cartStorage) {
-        currentCart = JSON.parse(cartStorage); //list of arrays in one variable
-        console.log(currentCart);
+        var currentCart = JSON.parse(cartStorage); //list of arrays in one variable
+        console.log("current cart [parsed]:", currentCart);
         for (var i= 0; i < currentCart.length; i++) {
-            addItemToCartPage();
+            addItemToCartPage(currentCart[i]);
         }
     }
     /* for item in currentCart:
@@ -67,39 +37,27 @@ function openCart() {
     */
 }
 
-function addItemToCartPage() {
-    for (var i= 0; i < currentCart.length; i++) {
-        var imgName = currentCart[i].img;
-        var colorName = currentCart[i].color;
-        var fillName = currentCart[i].fill;
-        var qtyNum = currentCart[i].qty;
-        var template =
-        `
-        <div class="cart-item">
-            <div class="cart-image">
-                <img src="${imgName}">
-            </div>
-            <div class="cart-description">
-              <p>Round Pillow, ${colorName}<br>
-              Fill: ${fillName}</p>
-            </div>
-            <div class="cart-qty">${qtyNum}</div>
-            <div class="cart-price">$30</div>
+function addItemToCartPage(item) {
+    console.log("adding item:", item);
+
+    var template =
+    `
+    <div class="cart-item">
+        <div class="cart-image">
+            <img src="${item.img}">
         </div>
-        `;
-        console.log(currentCart);
-        var holder = document.getElementById('cartItemHolder');
-        if (holder) {
-            var element = document.createElement('div');
-            element.innerHTML = template;
-            holder.appendChild(element);
-        }
+        <div class="cart-description">
+          <p>Round Pillow, ${item.color}<br>
+          Fill: ${item.fill}</p>
+        </div>
+        <div class="cart-qty">${item.qty}</div>
+        <div class="cart-price">$30</div>
+    </div>
+    `;
+    var holder = document.getElementById('cartItemHolder');
+    if (holder) {
+        var element = document.createElement('div');
+        element.innerHTML = template;
+        holder.appendChild(element);
     }
 }
-
- window.addEventListener("load", function(event) {
-    openCart();
-  });
-
-
-
